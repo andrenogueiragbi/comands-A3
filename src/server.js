a6ac1c2405699b62481fdb1ee727b6ef85953910
andrenogueiragbi <andrepereiragbi@gmail.com>
2022-06-03 21:09:42 -0300

    add deploy no heroku

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/v1',routes);
 
-app.listen(5000, () =>{
+app.listen(process.env.PORT || 5000, () =>{
     console.log("Rodando http na porta 5000 \\O/.");
 })
